29caf41eca3463e4b58d2a8567c35e4196a70bf8
Andrew Rhee <adrhee@gmail.com>
2016-12-17 12:04:23 -0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function() {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'andrew',
-    text: 'Hey. This is Andrew'
-  });
 });
 
 socket.on('disconnect', function() {
